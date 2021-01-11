7430fecb2282c7ad4eab535a41e8144aa9576183
bullhornfixie <dawson.jon@hotmail.com>
2021-01-11 16:02:19 +0000

    step complete - top up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+  
+  it "tops up the balance" do
+    subject.topup(4)
+    expect(subject.balance).to eq(4)
+  end
+  
 end
+
