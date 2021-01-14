5cc730a4e5008f3019806cf13a6108f5e09e5fc3
bullhornfixie <dawson.jon@hotmail.com>
2021-01-14 13:43:32 +0000

    Refactor - final part of step 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :in_journey, :entry_station
 
   MINIMUM_BALANCE = 1
   MAXIMUM_BALANCE = 90
@@ -7,6 +7,7 @@ class Oystercard
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -18,8 +19,9 @@ class Oystercard
     @balance + amount > MAXIMUM_BALANCE
   end
   
-  def touch_in
+  def touch_in(station)
     raise "Insufficient funds" if @balance < MINIMUM_BALANCE
+    @entry_station = "Unknown"
     @in_journey = true
   end
   
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,21 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
-  
   let(:minimum) { Oystercard::MINIMUM_BALANCE }
   let(:maximum) { Oystercard::MAXIMUM_BALANCE }
+  let(:station) { double :station }
   
   context 'When initializing an Oystercard' do
     it { is_expected.to respond_to(:balance) }                            
     it { is_expected.to respond_to(:in_journey) }
-    
+    it { is_expected.to respond_to(:entry_station) }
+
     it 'Should have a balance of 0' do
       expect(subject.balance).to eq(0)
     end
   end
   
   describe 'top_up' do
-    
     it 'Increases the balance of the card' do
       subject.top_up(10)
       expect(subject.balance).to eq(10)
@@ -27,7 +27,6 @@ describe Oystercard do
   end
   
   describe 'deduct' do
-   
     it 'Deducts fare from card balance' do
       subject.top_up(maximum)
       subject.send(:deduct, minimum) # syntax for private method access
@@ -36,38 +35,49 @@ describe Oystercard do
   end
   
   describe 'touch_in' do 
+    before(:each) do
+      subject.top_up(minimum)
+      subject.touch_in(station)
+    end
     
     it 'Updates the in_journey instance variable to true' do
-      subject.top_up(minimum)
-      subject.touch_in
       expect(subject).to be_in_journey 
     end
   
-    it 'Need to have minimum balance of £1 to travel' do
-      expect { subject.touch_in }.to raise_error "Insufficient funds"
+    it 'Remembers the entry station on touch in' do 
+      expect(subject.entry_station).to eq "Unknown"
+    end
+  end
+  
+  describe 'touch_in error' do
+    it 'Card needs to have minimum balance of £1 to travel' do
+      expect { subject.touch_in(station) }.to raise_error "Insufficient funds"
     end
   end
     
   describe 'touch_out' do 
-    
-    it 'Updates the in_journey instance variable to false' do
+    before(:each) do
       subject.top_up(10)
       subject.touch_out
+    end
+    
+    it 'Updates the in_journey instance variable to false' do
       expect(subject).not_to be_in_journey 
     end
     
     it 'Reduces the balance by minimum fare on touching out' do
-      subject.top_up(10)
-      subject.touch_out
       expect { subject.touch_out }.to change{ subject.balance }.by(-minimum)
+    end
+    
+    it 'Forgets the entry_station on touch out' do
+      expect(subject.entry_station).to eq nil
     end
   end
 
   describe 'in_journey?' do 
-    
     it 'Returns true of false based on journey status' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject.in_journey?).to eq(true)
     end
   end
