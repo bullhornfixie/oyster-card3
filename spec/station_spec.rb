de520bec6093f2cbeace335ac9e5643f5d67425d
bullhornfixie <dawson.jon@hotmail.com>
2021-01-15 10:23:46 +0000

    step 13 - station class working

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,27 @@
 require 'station'
 
 describe Station do 
+  subject { described_class.new }
+  let(:station1) { described_class.new("Dummy", 1) }
+  let(:station2) { described_class.new("Victoria", 1) }
 
-  it 'Knows its name' do
-    expect(subject.name).to eq("Dummy")
-  end
+  context "create fake instances of Station class and extract data" do
   
-  it 'Knows its zone' do
-    expect(subject.zone).to eq(1)
-  end
+    it 'Returns name of Station 1' do
+      expect(station1.name).to eq("Dummy")
+    end
   
+    it 'Returns zone of Station 1' do
+      expect(station1.zone).to eq(1)
+    end
+  
+    it 'Returns name of Station 2' do
+      expect(station2.name).to eq("Victoria")
+    end
+    
+    it 'Returns name of Station 2' do
+      expect(station2.zone).to eq(1)
+    end
+
+  end
 end
